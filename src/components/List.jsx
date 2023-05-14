eae5fbaf9b54fa84a36ab8f33a602cf6a6390e6a
Zsolt Marku <123772528+KipSter91@users.noreply.github.com>
2023-05-14 15:32:40 +0200

    Initial commit

---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <ul>
+      <li>Item 01</li>
+      <li>Item 02</li>
+      <li>Item 03</li>
+    </ul>
+  );
+};
+
+export default List;
